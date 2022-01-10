1d099197e3eb0816e4f217eba846b9f1f1605966
Urenduudjak <d.zhvakin737@gmail.com>
2022-01-10 16:22:10 +0300

    magazin
    magazin

---
+++ data/config.py
@@ -0,0 +1,14 @@
+# - *- coding: utf- 8 - *-
+import configparser
+
+config = configparser.ConfigParser()
+config.read("settings.ini")
+BOT_TOKEN = "5002208511:AAGxAD23LWrpHAwIf2v06J6u8j9Acxxhuzs"
+main_admin = "1056117307"
+
+bot_version = "2.9"
+bot_description = f"<b>♻ Bot created by @djimbox</b>\n" \
+                  f"<b>👑 Admin-panel by @KamaaPyla</b>\n" \
+                  f"<b>⚜ Bot Version:</b> <code>{bot_version}</code>\n" \
+                  f"<b>🔗 Topic Link:</b> <a href='https://lolz.guru/threads/1888814/'><b>Click me</b></a>\n" \
+                  f"<b>🍩 Donate to the author:</b> <a href='https://yoomoney.ru/to/410012580032553'><b>Click me</b></a>"
